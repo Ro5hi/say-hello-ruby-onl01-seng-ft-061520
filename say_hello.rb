16628d8c4c0b3d8b6f29648e2546dd415758fa5a
beautiful-instructions-5625 <slen.detablan@gmail.com>
2020-06-15 19:28:07 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,7 +1,7 @@
 # Build your say_hello method here
 
-say_hello("Kent Beck", language="Ruby Programmer")
+say_hello(name, language)
 
 say_hello("Kent Beck")
 
-say_hello("Kent Beck", )
+say_hello("Kent Beck", "Ruby Programmer")
