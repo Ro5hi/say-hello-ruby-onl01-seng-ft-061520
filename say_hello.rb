1eab8cd9d1f8605720f6ecf0703e2591217efda7
beautiful-instructions-5625 <slen.detablan@gmail.com>
2020-06-15 21:14:31 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,6 +1,6 @@
 # Build your say_hello method here
 
-def say_hello(name = "Kent Beck", language = "Ruby Programmer")
+say_hello(name = "Kent Beck", language = "Ruby Programmer")
   puts "Hello, #{name}!"
 end
 
